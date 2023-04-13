d816887daf8480c9eabb5a69b3b9ec17e163cdc9
Chintan Bhalodiya <53387537+Chintan-24@users.noreply.github.com>
2023-04-13 15:11:32 +0530

    Update HomeScreen.js

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -10,9 +10,9 @@ export default function HomeScreen({ createCall, startHairCheck }) {
 
   return (
     <div className="home-screen">
-      <h1>Daily React custom video application</h1>
-      <p>Start the demo with a new unique room by clicking the button below.</p>
-      <button onClick={startDemo} type="button">Click to start a call</button>
+      <h1>React video meeting application</h1>
+      <p>Create room by clicking the button below.</p>
+      <button onClick={startDemo} type="button">Start a call</button>
       <p className="small">Select “Allow” to use your camera and mic for this call if prompted</p>
     </div>
   );
